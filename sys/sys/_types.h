72b7adb20f165d76889e3872d65f574084d1d132
Marco Peereboom <marco@conformal.com>
2012-07-20 11:47:01 -0500

    add rune stuff to _types.h

--- sys/sys/_types.h
+++ sys/sys/_types.h
@@ -60,6 +60,29 @@ typedef __uint64_t	__fsblkcnt_t;	/* file system block count */
 typedef __uint64_t	__fsfilcnt_t;	/* file system file count */
 
 /*
+ * Unusual type definitions.
+ */
+/*
+ * rune_t is declared to be an ``int'' instead of the more natural
+ * ``unsigned long'' or ``long''.  Two things are happening here.  It is not
+ * unsigned so that EOF (-1) can be naturally assigned to it and used.  Also,
+ * it looks like 10646 will be a 31 bit standard.  This means that if your
+ * ints cannot hold 32 bits, you will be in trouble.  The reason an int was
+ * chosen over a long is that the is*() and to*() routines take ints (says
+ * ANSI C), but they use __ct_rune_t instead of int.
+ *
+ * NOTE: rune_t is not covered by ANSI nor other standards, and should not
+ * be instantiated outside of lib/libc/locale.  Use wchar_t.  wint_t and
+ * rune_t must be the same type.  Also, wint_t should be able to hold all
+ * members of the largest character set plus one extra value (WEOF), and
+ * must be at least 16 bits.
+ */
+typedef int		__ct_rune_t;    /* arg type for ctype funcs */
+typedef __ct_rune_t	__rune_t;       /* rune_t (see above) */
+typedef __ct_rune_t	__wint_t;       /* wint_t (see above) */
+typedef __uint32_t	__fixpt_t;      /* fixed point number */
+
+/*
  * mbstate_t is an opaque object to keep conversion state, during multibyte
  * stream conversions. The content must not be referenced by user programs.
  */
